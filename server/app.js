bde3ef86746e8df1f58d1cbb83bf9165108f6d89
MrBista <gustibisman2308@gmail.com>
2023-02-18 18:42:47 +0700

    port repear

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 const routes = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
